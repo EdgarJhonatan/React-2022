868bab260d7c6e147e805a8eb39835a7940e0e30
Edgar Delgadillo <jdmkrd@gmail.com>
2022-04-09 18:57:56 -0500

    GifExpert terminado

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,34 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-   const [images, setImages] = useState([])
-
-   useEffect( () => {
-      getGifs();
-   }, [] )
-
-   const getGifs = async() => {
-      const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Mory&limit=10&api_key=ik3O7lRCmyBbvrRrkn4PErcAcRmnEkq9';
-      const resp = await fetch( url );
-      const { data } = await resp.json();
-
-      const gifs = data.map( img => {
-         return {
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url
-         }
-      })
-      console.log(gifs);
-      setImages( gifs );
-   }
-
+   const { data:images, loading } = useFetchGifs( category );
 
    return (
       <>
-         <h3> {category} </h3>
+         <h3 className='animate__animated animate__fadeIn'> {category} </h3>
+         { loading && <p className='animate__animated animate__flash'>Loading</p>}
+      
          <div className='card-grid'>
             {
                images.map( img => (
@@ -38,7 +20,7 @@ export const GifGrid = ({ category }) => {
                   />
                ))
             }
-         </div>
+         </div>        
       </>
    )
 }
