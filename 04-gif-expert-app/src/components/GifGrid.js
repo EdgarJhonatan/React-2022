30c5a6e32bcd405639e51002c4bccc6c5371b0e1
Edgar Delgadillo <jdmkrd@gmail.com>
2022-05-12 12:16:59 -0500

    termino de clase 4

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs'
-import { GifGridItem } from './GifGridItem';
+import React from "react";
+import PropTypes from "prop-types";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-
    const { data: images, loading } = useFetchGifs(category);
 
    return (
       <>
-         <h3 className="animate__animated animate__fadeIn">{category}</h3>
-         {loading && <p className="animate__animated animate__flash">Loading...</p>}
+         <h3 className="animate__animated animate__fadeIn">
+            {category}
+         </h3>
+         {loading && (
+            <p className="animate__animated animate__flash">
+               Loading...
+            </p>
+         )}
          <div className="row justify-content-center h-100">
-            {
-               images.map(img => <GifGridItem key={img.id} {...img} />)
-            }
+            {images.map((img) => (
+               <GifGridItem key={img.id} {...img} />
+            ))}
          </div>
       </>
-   )
-}
+   );
+};
+
+GifGrid.propTypes = {
+   category: PropTypes.string.isRequired,
+};
--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
 export const GifGridItem = ({ title, url }) => {
-  return (
-    <div className="col-md-3 col-sm-6 col-10 mb-4  animate__animated animate__fadeIn">
-      <div className="card h-100" >
+   return (
+      <div className="col-md-3 col-sm-6 col-10 mb-4  animate__animated animate__fadeIn">
+         <div className="card h-100">
+            <img className="card-img-top" src={url} alt={title} />
 
-        <img className="card-img-top" src={url} alt={title} />
-
-        <div className="card-body">
-          <p className="card-text"> {title}</p>
-        </div>
-
+            <div className="card-body">
+               <p className="card-text"> {title}</p>
+            </div>
+         </div>
       </div>
-    </div>
-  )
-}
+   );
+};
+
+GifGridItem.propTypes = {
+   title: PropTypes.string.isRequired,
+   url: PropTypes.string.isRequired,
+};
