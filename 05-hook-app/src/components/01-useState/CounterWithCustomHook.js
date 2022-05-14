de18e1cade537d177764baefd7ca18941dee9f2d
Edgar Delgadillo <jdmkrd@gmail.com>
2022-05-14 18:58:45 -0500

    clase 05

---
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useCouter } from "../hooks/useCouter";
+import "./counter.css";
+export const CounterWithCustomHook = () => {
+   const { state, increment, decrement, reset } = useCouter(100);
+
+   return (
+      <>
+         <h1>Counter with Hook: {state}</h1>
+         <hr />
+
+         <button
+            onClick={() => increment(2)}
+            className="btn btn-primary">
+            + 1
+         </button>
+
+         <button onClick={reset} className="btn btn-primary">
+            Reset
+         </button>
+
+         <button
+            onClick={() => decrement(1)}
+            className="btn btn-primary">
+            - 1
+         </button>
+      </>
+   );
+};
---
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MultipleCustomHooks = () => {
+   return (
+      <div>
+         <h1>Custom Hooks!!!</h1>
+      </div>
+   );
+};
